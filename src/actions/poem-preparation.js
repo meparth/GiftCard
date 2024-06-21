f685af54f56855edd3a61de1185cc6c82256b986
meparth <meparthaprotim@gmail.com>
2024-06-21 20:07:05 +0600

    add mistral

--- src/actions/poem-preparation.js
+++ src/actions/poem-preparation.js
@@ -15,10 +15,9 @@ export const getPreparedPromptWithInputData = (inputData) => {
     const {recipient, messageTitle, interests, occasion: {value: occasionText}} = inputData
 
     const interestsAsText = interests.reduce((prev, curr) => prev + ', ' + curr.value, '').substring(1)
-    console.log(interestsAsText, recipient, messageTitle, occasionText)
 
     const prompt = `I want to get a poem for someone. Write a 4 line poem for a person named ${recipient}. I want the poem to greet him on his ${occasionText}.
-                             consider that their interests are: ${interestsAsText}. Use appropriate emojis at the start and end of each line. Make it cordial. Give me only the poem, NOTHING ELSE.`
+                             consider that their interests are: ${interestsAsText}. Use appropriate emojis at the start and end of each line. Make it cordial and happy.
+                             the title of the poem should be - ${messageTitle}. Give me only the poem, NOTHING ELSE.`
     return getSanitizedText(prompt)
-    //interests: Array(4), occasion: {…}, recipient: 'abcd', messageTitle: 'happy birthday from sylhet'
 }
--- src/actions/text-generation.js
+++ src/actions/text-generation.js
@@ -1,15 +1,24 @@
 "use server"
 
-
+import MistralClient from '@mistralai/mistralai';
 
 import {getPreparedPromptWithInputData} from "@/actions/poem-preparation";
-
 export const textGeneration = async (prompt) => {
 
+    const apiKey = process.env.mistral_api_key;
+
+    const client = new MistralClient(apiKey);
+
+    const chatResponse = await client.chat({
+        model: 'mistral-large-latest',
+        messages: [{role: 'user', content: prompt}],
+    });
+
+    console.log('response: ', chatResponse.choices[0].message.content);
 }
 
 export const startGeneration = async (input) => {
     const preparedPrompt = getPreparedPromptWithInputData(input)
     console.log('---generating for prompt: ', preparedPrompt, '---')
-
+    textGeneration(preparedPrompt)
 }
